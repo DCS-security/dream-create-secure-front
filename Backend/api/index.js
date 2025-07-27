5e21102291437f3cec1429203bb1035274412ade
DCS-security <devanshcybersec1234@gmail.com>
2025-07-27 11:15:37 +0000

    Backend Changes

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const mongoose = require("mongoose");
 // const contactRoutes = require('./routes/contact');
-
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 
@@ -16,5 +16,8 @@ mongoose
 // Route for contact form
 // app.use('/api/contact', contactRoutes);
 app.use("/api/contact", require("./routes/contact"));
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at ${PORT}`);
+});
 module.exports = app;
 
